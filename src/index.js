f6e8c226210a3130ce8cd09725d10c53706b5800
Jan-André Zinser <jan-andre.zinser@bertelsmann.de>
2016-02-24 19:35:34 +0100

    export actions from react-router-redux to keep that package completely in the background

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ export { fetch, prefetch, defer } from './decorator';
 
 // export createStore
 export { createStore } from './createStore';
+
+// export react-router-redux actions
+export { push as pushAction, replace as replaceAction, go as goAction, goBack as goBackAction, goForward as goForwardAction } from 'react-router-redux';
