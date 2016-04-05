4c0e2adc7c840ddbeb1c56ada8b1e56209f36ef3
Jan-André Zinser <jan-andre.zinser@bertelsmann.de>
2016-04-06 00:07:07 +0200

    Bugfix: load initial state without server side rendering

--- src/client.js
+++ src/client.js
@@ -1,10 +1,27 @@
 import { match } from 'react-router';
 import loadClientState from './fetch/loadClientState';
 
-export default function client({ store, initialState, routes, history, getLocals }, cb) {
+export default function client(props, cb) {
+  const { store, initialState, routes, history, getLocals } = props;
   match({ routes, history }, (error, redirectLocation, renderProps) => {
-    loadClientState({ store, initialState, routes, history, getLocals });
-
-    cb(error, redirectLocation, renderProps);
+    if (redirectLocation) {
+      history.replace(redirectLocation);
+      client(props, cb);
+    } else if (renderProps) {
+      const data = {
+        store,
+        initialState,
+        routes,
+        history,
+        getLocals,
+        redirectLocation,
+        renderProps,
+      };
+      loadClientState(data, (err) => {
+        cb(err, redirectLocation, renderProps);
+      });
+    } else {
+      cb(error, redirectLocation, renderProps);
+    }
   });
 }
--- src/fetch/loadClientState.js
+++ src/fetch/loadClientState.js
@@ -8,16 +8,31 @@ const shouldFetch = (oldLocation, newLocation) => !oldLocation && newLocation ||
 
 const log = (e) => {
   if (process.env.NODE_ENV !== 'production') {
-    console.error(e);
+    console.error(e); // eslint-disable-line no-console
   }
 };
 
-export default function ({ history, routes, store, getLocals, initialState = {} }) {
-  let oldLocation = null;
-  let initialRender = true;
-  const useState = !isEmpty(initialState);
+const resolveDataDependencies = (components, getAllLocals) =>
+  Promise
+    .resolve()
+    .then(() => getDataDependencies(PREFETCH, components, getAllLocals))
+    .then(() => {
+      // call fetch but doesn't wait for it
+      getDataDependencies(FETCH, components, getAllLocals)
+        .catch(log);
 
-  const stopResolving = history.listenBefore((location, continueTransition) => {
+      // call defer but doesn't wait for it
+      getDataDependencies(DEFER, components, getAllLocals)
+        .catch(log);
+
+      return Promise.resolve();
+    });
+
+
+const registerHook = ({ history, routes, store, getLocals, location }) => {
+  let oldLocation = location;
+
+  history.listenBefore((location, continueTransition) => { // eslint-disable-line no-shadow
     if (!shouldFetch(oldLocation, location)) {
       continueTransition();
       return;
@@ -31,38 +46,40 @@ export default function ({ history, routes, store, getLocals, initialState = {} 
         const { components } = renderProps;
         const getAllLocals = createGetLocals(renderProps, store, getLocals);
 
-        // use initial state on first render when server rendered with state for the current url
-        const useInitialState = initialRender && useState && !shouldFetch(initialState.routing.location, location); // eslint-disable-line max-len
-
-        Promise
-          .resolve()
-          .then(() => {
-            if (!useInitialState) {
-              return getDataDependencies(PREFETCH, components, getAllLocals);
-            }
-            return Promise.resolve();
-          })
-          .then(() => {
-            if (!useInitialState) {
-              // call fetch but doesn't wait for it
-              getDataDependencies(FETCH, components, getAllLocals)
-                .catch(log);
-            }
-
-            // call defer but doesn't wait for it
-            getDataDependencies(DEFER, components, getAllLocals)
-              .catch(log);
-
-            return Promise.resolve();
-          })
+        resolveDataDependencies(components, getAllLocals)
           .then(continueTransition, continueTransition)
           .catch(log);
       } else {
         continueTransition();
       }
-      initialRender = false;
     });
   });
+};
 
-  return stopResolving;
+
+export default function ({ history, routes, store, getLocals, initialState = {}, renderProps }, cb) { // eslint-disable-line max-len
+  const hasInitialState = !isEmpty(initialState);
+
+  // use synchronous listen to get current url -> changes with history v3
+  history.listen((location) => {
+    const hooksData = { history, routes, store, getLocals, location };
+
+    if (!hasInitialState) {
+      const { components } = renderProps;
+      const getAllLocals = createGetLocals(renderProps, store, getLocals);
+
+      resolveDataDependencies(components, getAllLocals)
+        .then(() => {
+          registerHook(hooksData);
+          cb();
+        })
+        .catch((err) => {
+          registerHook(hooksData);
+          cb(err);
+        });
+    } else {
+      registerHook(hooksData);
+      cb();
+    }
+  })();
 }
